13def321514c1ef1b57707774fbc55833a4999ba
Donchess1 <stepabod@yahoo.com>
2025-02-13 22:29:08 +0100

    edit test.yml

--- api/routes/books.py
+++ api/routes/books.py
@@ -52,8 +52,8 @@ async def get_books() -> OrderedDict[int, Book]:
 async def get_book(book_id: int) -> Book:
     book = db.get_book(book_id)
 
-    #if not book:
-    raise HTTPException(status_code=404, detail="Book not found")
+    if not book:
+        raise HTTPException(status_code=404, detail="Book not found")
     
     return JSONResponse(
         status_code=status.HTTP_200_OK,
